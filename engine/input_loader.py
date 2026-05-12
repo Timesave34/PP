1b0451f625c53d8fae7704eec7a51883a5c19846
Timesave34 <timesave34@gmail.com>
2026-05-12 18:06:42 +0100

    Update input file path and clean up comments

--- engine/input_loader.py
+++ engine/input_loader.py
@@ -1,12 +1,11 @@
 import pandas as pd
-
-#EXCEL_PATH = "./inputs.xlsm"
 # ------------------------------------------------
 # LOAD INPUT file values
 # if running on pc "C:\Users\ianbe\PyPen1\pension_inputs.xlsm"
 # if running via github "./pension_inputs.xlsm"
 # ------------------------------------------------
-EXCEL_PATH=r"C:\Users\ianbe\PyPen1\pension_inputs.xlsm"
+#EXCEL_PATH=r"C:\Users\ianbe\PyPen1\pension_inputs.xlsm"
+EXCEL_PATH = "./inputs.xlsm"
 
 def load_inputs():
     df = pd.read_excel(
